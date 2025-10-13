1b580ba2d8526de5b12ba7914aad6d29a5f13915
xirzo <easttem@gmail.com>
2025-10-13 16:03:40 +0300

    feat: add responsive github icon

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '@fortawesome/fontawesome-free/css/all.css';
 import './app/global.css'
 import App from './app/App.tsx'
+
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
--- src/widgets/footer/footer.tsx
+++ src/widgets/footer/footer.tsx
@@ -4,7 +4,11 @@ function Footer() {
     return (
         <footer className="footer">
             <div className="footer-content">
-                {new Date().getFullYear()} &middot; By <a href="https://github.com/xirzo" target="_blank" rel="noopener noreferrer">xirzo</a>
+                {new Date().getFullYear()} &middot;
+                By <a href="https://github.com/xirzo" target="_blank" rel="noopener noreferrer">
+                    <i className="fa-brands fa-github" />
+                    xirzo
+                </a>
             </div>
         </footer>
     )
