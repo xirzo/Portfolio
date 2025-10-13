c52ccefb88ccada7fc790503b6dedfa74bebc46b
xirzo <easttem@gmail.com>
2025-10-13 16:26:53 +0300

    feat: add wide row

--- src/pages/mainPage.tsx
+++ src/pages/mainPage.tsx
@@ -12,6 +12,31 @@ function MainPage() {
             </AnimatedHover>
 
             <div className="grid-container">
+                <Card>
+                    <h1>Sharp heading</h1>
+                    <p>A muted shade for the rest of the text.</p>
+                    <Button>
+                        Some Action
+                    </Button>
+                </Card>
+
+                <Card>
+                    <h1>Sharp heading</h1>
+                    <p>A muted shade for the rest of the text.</p>
+                    <Button>
+                        Some Action
+                    </Button>
+                </Card>
+
+                <div className="wide-row">
+                    <Card>
+                        <h1>Sharp heading</h1>
+                        <p>A muted shade for the rest of the text.</p>
+                        <Button>
+                            Some Action
+                        </Button>
+                    </Card>
+                </div>
 
                 <Card>
                     <h1>Sharp heading</h1>
